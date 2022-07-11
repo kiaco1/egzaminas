6f0eb8d979ebc2d3dae0de99f87a538c66624f3e
Juodis <Mantas.Juodis@dematic.com>
2022-07-11 08:49:48 +0200

    1-2

--- JS Tasks/Task 1/script.js
+++ JS Tasks/Task 1/script.js
@@ -8,3 +8,26 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+function addDiv() {
+
+
+}
+
+document.forms[0].addEventListener("submit", (e) => {
+    e.preventDefault();
+    const mass = document.getElementById("search").value;
+    const div0 = document.createElement("div");
+    const div1 = document.createElement("div");
+    const div2 = document.createElement("div");
+    div0.textContent = "Svarai (lb): " + mass * 2.2046;
+    div1.textContent = "Gramai (g): " + mass / 0.0010000;
+    div2.textContent = "Uncijos (oz) " + mass * 35.274;
+    const output = document.getElementById("output");
+    output.appendChild(div0);
+    output.appendChild(div1);
+    output.appendChild(div2);
+    div0.className = "style";
+    div1.className = "style";
+    div2.className = "style";
+})
